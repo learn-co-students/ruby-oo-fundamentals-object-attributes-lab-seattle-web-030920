0f0cdafbfd643169bb17b277063449a89a2e0dd4
djknudson <djknudson@icloud.com>
2020-03-10 18:07:42 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Person
+    def initialize
+    end
+
+    def name
+        @name
+    end
+    def name=(new_name)
+        @name = new_name
+    end
+
+    def job
+        @job
+    end
+    def job=(new_job)
+        @job = new_job
+    end
+end
